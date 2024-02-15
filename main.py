3e6868b99c0e24da4890207855053254f5a46d48
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-02-15 13:18:25 +0300

    Add testing web server

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from loguru import logger
+logger.info("Starting...")
+
 import time
 import configparser
-from loguru import logger
+import web_server_test as web_server
 
 if __name__ == '__main__':
     startTime = time.localtime(time.time())
     strStartTime = f"{startTime.tm_year}_{startTime.tm_mon}_{startTime.tm_mday}_{startTime.tm_hour}-{startTime.tm_min}-{startTime.tm_sec}"
 
-    logger.info(f"Initialization date: {strStartTime}")
     logger.add(f"logs/logging_{strStartTime}.log", level="INFO", format="{time} {level} {message}")
 
     config = configparser.ConfigParser()
     config.read("server.ini")
-
-    logger.info(f"Start SOCKET")
-    logger.info(f"Port:{config['SOCKETConfig']['Port']}")
+    SERVER_PORT = int(config['SOCKETConfig']['Port'])
+    
+    web_server.start_server(SERVER_PORT)
 
 
