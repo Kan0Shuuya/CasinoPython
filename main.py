631aef60181cd66cb72433b4f175917806d2e424
Aether <59086566+AetherMagee@users.noreply.github.com>
2024-02-15 12:46:34 +0300

    Replace logging with loguru

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import logging
 import time
 import configparser
+from loguru import logger
 
 if __name__ == '__main__':
     startTime = time.localtime(time.time())
     strStartTime = f"{startTime.tm_year}_{startTime.tm_mon}_{startTime.tm_mday}_{startTime.tm_hour}-{startTime.tm_min}-{startTime.tm_sec}"
-    logging.basicConfig(level=logging.INFO, filename=f"logs/logging_{strStartTime}.log", filemode="w")
-    config = configparser.ConfigParser()
-    logging.info(f"Initialization date: {strStartTime}")
-    logging.info(f"Start SOCKET")
-    config.read("server.ini")
-    logging.info(f"Port:{config['SOCKETConfig']['Port']}")
 
+    logger.info(f"Initialization date: {strStartTime}")
+    logger.add(f"logs/logging_{strStartTime}.log", level="INFO", format="{time} {level} {message}")
+
+    config = configparser.ConfigParser()
+    config.read("server.ini")
+
+    logger.info(f"Start SOCKET")
+    logger.info(f"Port:{config['SOCKETConfig']['Port']}")
 
 
