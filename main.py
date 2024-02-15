23431f561b2ce0bcd660be3bca9f930861cbd961
KanoWare <trolili464@gmail.com>
2024-02-15 12:42:25 +0600

    Start Project

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+import time
+import configparser
+
+if __name__ == '__main__':
+    startTime = time.localtime(time.time())
+    strStartTime = f"{startTime.tm_year}_{startTime.tm_mon}_{startTime.tm_mday}_{startTime.tm_hour}-{startTime.tm_min}-{startTime.tm_sec}"
+    logging.basicConfig(level=logging.INFO, filename=f"logs/logging_{strStartTime}.log", filemode="w")
+    config = configparser.ConfigParser()
+    logging.info(f"Initialization date: {strStartTime}")
+    logging.info(f"Start SOCKET")
+
+
+
+
